2a5f5510af61400416cb20ea0ddd37956d55d89b
heinrich85 <maxkam3928@gmail.com>
2024-07-27 09:51:38 +0300

    b

---
+++ app/keyboards/films.py
@@ -0,0 +1,18 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+
+
+# Inline клавіатура для списку фільмів
+def build_films_keyboard(films: list):
+   builder = InlineKeyboardBuilder()
+   for index, film in enumerate(films):
+       builder.button(text=film.get("title"), callback_data=f"film_{index}")
+   return builder.as_markup()
+
+
+def build_film_details_keyboard(url):
+   builder = InlineKeyboardBuilder()
+   builder.button(text="Перейти за посиланням", url=url)
+   builder.button(text="До списку фільмів", callback_data="back")
+   return builder.as_markup()
